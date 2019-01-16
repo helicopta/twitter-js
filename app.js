4f41be0655c781aabc94785b6fe5a6873803f44b
wangdn90@gmail.com <helicopta@Dannis-MacBook-Air.local>
2019-01-15 21:17:06 -0500

    git set up

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require( 'express' );
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/ha/',function (req, res, next) {
+  console.log(req.url,req.method,res.statusCode)
+  next()
+});
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/ha/bu/', (req, res) => res.send('Bye World!'));
+
 
 app.listen(port,()=>console.log('server listening'))
