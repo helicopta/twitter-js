1c6c8ba42065bc6b84a39ee7611eb83bd0b18b6d
wangdn90@gmail.com <helicopta@Dannis-MacBook-Air.local>
2019-01-20 21:23:52 -0500

    route set up

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require( 'express' );
+const nunjucks = require( 'nunjucks');
 const app = express();
+const routes = require('./routes');
+
 const port = 3000;
 
-app.use('/ha/',function (req, res, next) {
-  console.log(req.url,req.method,res.statusCode)
-  next()
-});
+// app.use('/ha/',function (req, res, next) {
+//   console.log(req.url,req.method,res.statusCode)
+//   next()
+// });
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/ha/bu/', (req, res) => res.send('Bye World!'));
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+
+app.use('/', routes);
+
+
+
+nunjucks.configure('views',{noCache: true});
+//nunjucks.render('index.html', locals);
 
 
 app.listen(port,()=>console.log('server listening'))
